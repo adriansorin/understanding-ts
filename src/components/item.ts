12feea2d4d60bbffa527b9b51e1a114fb45fd9a8
Adrian Avram <adrianavram@Adrians-MacBook-Pro.local>
2020-03-01 19:28:25 +0200

    add getter

--- src/components/item.ts
+++ src/components/item.ts
@@ -7,6 +7,10 @@ export default class ProjectItem extends BaseComponent<
 > {
   private project!: Project;
 
+  get persons() {
+    return this.project.people === 1 ? '1 person' : `${this.project.people} persons`;
+  }
+
   constructor(hostID: string, project: Project) {
     super("single-project", hostID, false, project.id);
     this.project = project;
@@ -19,7 +23,7 @@ export default class ProjectItem extends BaseComponent<
 
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent = this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + ' assigned';
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
